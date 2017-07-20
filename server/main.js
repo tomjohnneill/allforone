f15e8b12f4701a77057b3281293a0845d532ffb9
Tom Neill <tomjohnneill@live.co.uk>
2017-07-20 17:10:26 +0100

    Imported the new methods

--- server/main.js
+++ server/main.js
@@ -1,11 +1,19 @@
 import { Meteor } from 'meteor/meteor';
 import { Pledges } from '../imports/api/pledges.js';
 import { Threads } from '../imports/api/threads.js';
+import { Interests } from '../imports/api/interests.js';
+import { Events } from '../imports/api/events.js';
+import { Meetups } from '../imports/api/meetups.js';
+import { Projects } from '../imports/api/projects.js';
+import { Contributions } from '../imports/api/contributions.js';
+import { Suggestions } from '/imports/api/suggestions.js';
 import { ServiceConfiguration } from 'meteor/service-configuration';
 import '../imports/api/pledgemethods.js';
 import '../imports/api/usermethods.js';
 import '../imports/api/threadmethods.js';
 import '../imports/api/alertmethods.js';
+import '../imports/api/interestmethods.js';
+import '../imports/api/projectmethods.js';
 
 Meteor.startup(() => {
 
