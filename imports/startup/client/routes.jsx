db886d610ac3438f5470aec5969bc7fa13383c0a
Tom Neill <tomjohnneill@live.co.uk>
2017-08-24 14:28:12 +0100

    me

--- imports/startup/client/routes.jsx
+++ imports/startup/client/routes.jsx
@@ -1,35 +1,90 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import Loadable from 'react-loadable';
 
 import { App } from '../../ui/layouts/app.jsx';
-import { Index } from '../../ui/components/index.jsx';
 import { NotFound } from '../../ui/pages/notfound.jsx';
 import { Pledge } from '../../ui/pages/pledge.jsx';
-import Profile from '../../ui/pages/profile.jsx';
 import UserTabs from '../../ui/components/tabs.jsx';
-import EditPledge from '../../ui/pages/editpledge.jsx';
 import PledgeList from '../../ui/pages/pledgelist.jsx';
 import DynamicPledge from '../../ui/pages/dynamicpledge.jsx';
-import EditThread from '../../ui/pages/editthread.jsx';
-import Thread from '../../ui/pages/thread.jsx';
-import Community from '../../ui/pages/community.jsx';
 import {Interests} from '../../ui/pages/interests.jsx'
-import PublicProfile from '../../ui/pages/publicprofile.jsx';
-
-import {Terms} from '../../ui/pages/terms.jsx';
-import {About } from '../../ui/pages/about.jsx';
-import {PrivacyPolicy} from '../../ui/pages/privacypolicy.jsx';
 
 import EmailAdmin from '../../ui/pages/emailadmin.jsx';
-import Analytics from '../../ui/pages/analytics.jsx'
 import Project from '../../ui/pages/project.jsx';
 import Discover from '../../ui/pages/discover.jsx'
-import PledgeAnalytics from '../../ui/pages/pledgeanalytics.jsx';
-
-import Admin from '../../ui/pages/admin.jsx';
+import PledgedUsers from '../../ui/pages/pledgedusers.jsx';
+import UserList from '../../ui/pages/userlist.jsx';
+import Groups from '../../ui/pages/groups.jsx';
+import Broadcast from '../../ui/pages/broadcast.jsx';
+import StripeRedirect from '../../ui/pages/striperedirect.jsx'
 
 import TestyPage from '../../ui/pages/testypage.jsx';
+
+const Loading = () => (
+  <div/>
+)
+
+const UserListLoadable = Loadable({
+  loader: () => import('/imports/ui/pages/userlist.jsx'),
+  loading: Loading
+});
+
+const FormBuilderLoadable = Loadable({
+  loader: () => import('/imports/ui/pages/formbuilder.jsx'),
+  loading: Loading
+});
+
+const EditPledgeLoadable = Loadable({
+  loader: () => import('/imports/ui/pages/editpledge.jsx'),
+  loading: Loading
+});
+
+const AnalyticsLoadable = Loadable({
+  loader: () => import('/imports/ui/pages/analytics.jsx'),
+  loading: Loading
+});
+
+const PledgeAnalyticsLoadable = Loadable({
+  loader: () => import('/imports/ui/pages/pledgeanalytics.jsx'),
+  loading: Loading
+});
+
+const AdminLoadable = Loadable({
+  loader: () => import('/imports/ui/pages/admin.jsx'),
+  loading: Loading
+});
+
+const PublicProfileLoadable = Loadable({
+  loader: () => import('/imports/ui/pages/publicprofile.jsx'),
+  loading: Loading
+});
+
+const TermsLoadable = Loadable({
+  loader: () => import('/imports/ui/pages/terms.jsx'),
+  loading: Loading
+});
+
+const AboutLoadable = Loadable({
+  loader: () => import('/imports/ui/pages/about.jsx'),
+  loading: Loading
+});
+
+const PrivacyPolicyLoadable = Loadable({
+  loader: () => import('/imports/ui/pages/privacypolicy.jsx'),
+  loading: Loading
+});
+
+const PaymentPlansLoadable = Loadable({
+  loader: () => import('/imports/ui/pages/paymentplans.jsx'),
+  loading: Loading
+});
+
+const MessagesLoadable = Loadable({
+  loader: () => import('/imports/ui/pages/messages.jsx'),
+  loading: Loading
+});
 
 /* This is obviously just the routing code
 Interesting things to note:
@@ -42,28 +97,32 @@ Meteor.startup( () => {
     <Router history={ browserHistory }>
       <Route path="/" component={ App }>
         <IndexRoute component={ UserTabs } />
-        <Route path='/terms' component={Terms}/>
+        <Route path='/terms' component={TermsLoadable}/>
         <Route path='/testypage' component={TestyPage}/>
-        <Route path='/privacypolicy' component={PrivacyPolicy}/>
-        <Route path='/admin' component={Admin}/>
-        <Route path='/community' component={Community}/>
+        <Route path='/privacypolicy' component={PrivacyPolicyLoadable}/>
+        <Route path='/messages' component={MessagesLoadable}/>
+        <Route path='/admin/:adminTab' component={AdminLoadable}/>
         <Route path='/emailadmin' component={EmailAdmin}/>
         <Route path='/:pledgeId/project' component={Project}/>
         <Route path='/discover' component={Discover}/>
-        <Route path='/analytics' component={Analytics}/>
+        <Route path='/stripe-redirect' component={StripeRedirect}/>
+        <Route path='/analytics' component={AnalyticsLoadable}/>
         <Route path='/interests' component={Interests}/>
-        <Route path='/about' component={About}/>
-        <Route path='/profile/:userId' component={PublicProfile}/>
+        <Route path='/about' component={AboutLoadable}/>
+        <Route path='/profile/:userId' component={PublicProfileLoadable}/>
         <Route path='/pages' component ={UserTabs} />
         <Route path='/pages/:tab' component={UserTabs}/>
         <Route path='/pages/pledges/new' component={PledgeList}/>
-        <Route path='/pages/community/:thread/:_id' component={Thread}/>
-        <Route path='/pages/community/:thread/:_id/edit' component={EditThread}/>
-        {/*<Route path='/pages/:tab/chat/:thread/:_id' component={DynamicThread}/>*/}
         <Route path='/pages/:tab/just-added/:pledge/:_id' component={UserTabs}/>
         <Route path='/pages/:tab/:pledge/:_id' component={DynamicPledge}/>
-        <Route path='/pages/:tab/:pledge/:_id/edit' component={EditPledge}/>
-        <Route path='/pages/:tab/:pledge/:_id/analytics' component={PledgeAnalytics}/>
+        <Route path='/pages/:tab/:pledge/:_id/edit' component={EditPledgeLoadable}/>
+        <Route path='/pages/:tab/:pledge/:_id/analytics' component={PledgeAnalyticsLoadable}/>
+        <Route path='/pages/:tab/:pledge/:_id/pledged-users' component={PledgedUsers}/>
+        <Route path='/pages/:tab/:pledge/:_id/user-list' component={UserListLoadable}/>
+        <Route path='/pages/:tab/:pledge/:_id/user-groups' component={Groups}/>
+        <Route path='/pages/:tab/:pledge/:_id/form-builder' component={FormBuilderLoadable}/>
+        <Route path='/pages/:tab/:pledge/:_id/broadcast' component={Broadcast}/>
+        <Route path='/pages/:tab/:pledge/:_id/payment-plans' component={PaymentPlansLoadable}/>
         <Route path='/pages/:tab/:pledge/:pledgeId/project' component={Project}/>
         <Route path='/pages/:tab/:pledge' component={UserTabs}/>
         <Route path="/pledge" component={ UserTabs } />
